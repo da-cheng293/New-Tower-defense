571e6b71ead8c31fc4d0eaa8330d0cbfe6137a7e
haimingz613 <54756484+haimingz613@users.noreply.github.com>
2020-06-25 18:18:10 -0700

    rules

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Game : MonoBehaviour
+{
+
+    public void OnStartGame1()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
+
+    
